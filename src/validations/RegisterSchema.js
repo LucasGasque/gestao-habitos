cebca38f8ff4618adc6adf70a25e0c9f4264775b
thaeki <pedrokopsch@gmail.com>
2021-10-15 10:46:30 -0300

    build: arquivo para validação de registro criado.

---
+++ src/validations/RegisterSchema.js
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  username: yup.string().required("Campo obrigatório."),
+  email: yup.string().required("Campo obrigatório.").email("Formato inválido."),
+  emailConfirm: yup
+    .string()
+    .required("Campo obrigatório.")
+    .oneOf([yup.ref("email"), null], "E-mails não conferem."),
+  password: yup
+    .string()
+    .required("Campo obrigatório.")
+    .matches(
+      "^(?=.*[a-z])(?=.*[0-9])(?=.{6,})",
+      "A senha precisa conter pelo menos uma letra maiúscula, minúscula e um número."
+    ),
+  passwordConfirm: yup
+    .string()
+    .required("Campo obrigatório")
+    .oneOf([yup.ref("password"), null], "Senhas não coincidem."),
+});
+
+export default schema;
