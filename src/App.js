0dc9986f666d010a584c82e4d678530e9b6a2902
thaeki <pedrokopsch@gmail.com>
2021-10-15 09:54:14 -0300

    build: pasta validations criada

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import GlobalStyle from './styles/global'
-import { ThemeProvider } from 'styled-components';
-import { themes } from './styles/themes';
-import { useState, useCallback } from 'react'
-import { ToastContainer } from 'react-toastify';
-import Providers from './providers';
-import Routes from './routes'
+import GlobalStyle from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./styles/themes";
+import { useState, useCallback } from "react";
+import { ToastContainer } from "react-toastify";
+import Providers from "./providers";
+import Routes from "./routes";
 
 const App = () => {
-
-  const [currentTheme, setCurrentTheme] = useState('light')
+  const [currentTheme, setCurrentTheme] = useState("light");
   const getOpositeTheme = useCallback(
-    ()=> (currentTheme === 'light' ? 'dark' : 'light'),
+    () => (currentTheme === "light" ? "dark" : "light"),
     [currentTheme]
-  )
+  );
 
   return (
-    <div >
+    <div>
       <Providers>
         <ThemeProvider theme={themes[currentTheme]}>
           <GlobalStyle />
           <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-            <Routes />
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <Routes />
         </ThemeProvider>
       </Providers>
     </div>
   );
-}
+};
 
 export default App;
