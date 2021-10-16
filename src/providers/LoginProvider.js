e24f03a93810f87dbff15cc36c3a3c15b67668b2
thaeki <pedrokopsch@gmail.com>
2021-10-16 00:37:44 -0300

    fix: ajuste na identação

--- src/providers/LoginProvider.js
+++ src/providers/LoginProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import api from "../services/api";
-import { toast } from 'react-toastify'
+import { toast } from "react-toastify";
 
 export const LoginContext = createContext();
 
@@ -11,19 +11,20 @@ export const LoginProvider = ({ children }) => {
 
   const handleLogin = ({ username, password }) => {
     const user = { username, password };
-    api.post("/sessions", user).then((response) => {
-      const { sucess } = response.data;
-      localStorage.setItem("@Login:token", JSON.stringify(sucess));
-      setAuthenticated(true);
-      history.push('/dasboard');
-    })
-    .catch((_) => toast.error('Senha ou e-mail incorretos.'))
+    api
+      .post("/sessions", user)
+      .then((response) => {
+        const { sucess } = response.data;
+        localStorage.setItem("@Login:token", JSON.stringify(sucess));
+        setAuthenticated(true);
+        history.push("/dasboard");
+      })
+      .catch((_) => toast.error("Senha ou e-mail incorretos."));
   };
 
   return (
-      <LoginContext.Provider
-      value={{authenticated, handleLogin}}>
-        {children}
-      </LoginContext.Provider>
-  )
+    <LoginContext.Provider value={{ authenticated, handleLogin }}>
+      {children}
+    </LoginContext.Provider>
+  );
 };
