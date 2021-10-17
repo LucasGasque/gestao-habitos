02c1c205a94cd514f54b8cb216f4c5b6bfd77c4c
thaeki <pedrokopsch@gmail.com>
2021-10-17 04:39:00 -0300

    build: provider de goals criado e adicionado à pasta providers

---
+++ src/providers/Goals/Goals.js
@@ -0,0 +1,62 @@
+import { createContext, useState, useEffect } from "react";
+import api from "../../services/api";
+import { toast } from "react-toastify";
+
+export const GoalsContext = createContext;
+
+export const GoalsProvider = ({ children }) => {
+  const token = JSON.parse(localStorage.getItem("@Login:token"));
+  const [goal, setGoal] = useState();
+
+  const getGoal = (id) => {
+    api
+      .get(`/goals/${id}/`)
+      .then((response) => setGoal(response))
+      .catch((err) => console.log(err));
+  };
+
+  const createGoal = (data) => {
+    api
+      .post("/goals/", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((_) => {
+        toast.info("Objetivo criado com sucesso!");
+      })
+      .catch((_) => toast.error("Algo deu errado."));
+  };
+
+  const updateGoal = (id, data) => {
+    api
+      .patch(`/goals/${id}/`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((_) => {
+        toast.info("Objetivo atualizado com sucesso!");
+      })
+      .catch((_) => toast.error("Algo deu errado."));
+  };
+
+  const deleteGoal = (id) => {
+    api
+      .delete(`/goals/${id}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((_) => {
+        toast.info("Objetivo deletado com sucesso!");
+      })
+      .catch((_) => toast.error("Algo deu errado."));
+  };
+
+  return (
+      <GoalsContext.Provider value={{deleteGoal,updateGoal,createGoal,getGoal,goal}}>
+          {children}
+      </GoalsContext.Provider>
+  )
+};
