b7780682f0770eae5d83ddf01bc6f07a3eff4038
thaeki <pedrokopsch@gmail.com>
2021-10-17 04:39:58 -0300

    fix: provider de goals adicionado ao arquivo root de providers

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,13 +1,16 @@
 import { LoginProvider } from "./Login/Login"
 import { RegisterProvider } from "./register/Register"
 import { HabitsProvider } from "./Habits/Habits";
+import { GoalsProvider } from "./Goals/Goals";
 
 const Providers = ({children}) => {
     return(
         <RegisterProvider>
             <LoginProvider>
                 <HabitsProvider>
+                    <GoalsProvider>
                 {children}
+                </GoalsProvider>
                 </HabitsProvider>
             </LoginProvider>
         </RegisterProvider>
