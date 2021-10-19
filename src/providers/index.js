32ae9e5fb0ef3687b96404a2009b66338bafa1de
Heric Felix <hfelixmartins@gmail.com>
2021-10-19 17:54:22 -0300

    build: Provider de categorias criado.

--- src/providers/index.js
+++ src/providers/index.js
@@ -4,21 +4,24 @@ import { HabitsProvider } from "./Habits/Habits";
 import { GoalsProvider } from "./Goals/Goals";
 import { GroupProvider } from "./Group/Group";
 import { ActivitiesProvider } from "./Activities/Activities";
+import { CategoriesContext } from "./Categories/Categories";
 
-const Providers = ({children}) => {
-    return(
-        <RegisterProvider>
-            <LoginProvider>
-                <HabitsProvider>
-                    <GoalsProvider>
-                      <GroupProvider>
-                        <ActivitiesProvider>{children}</ActivitiesProvider>
-                       </GroupProvider>
-                    </GoalsProvider>
-                </HabitsProvider>
-            </LoginProvider>
-        </RegisterProvider>
-    );
-}
+const Providers = ({ children }) => {
+  return (
+    <RegisterProvider>
+      <LoginProvider>
+        <HabitsProvider>
+          <GoalsProvider>
+            <GroupProvider>
+              <ActivitiesProvider>
+                <CategoriesContext>{children}</CategoriesContext>
+              </ActivitiesProvider>
+            </GroupProvider>
+          </GoalsProvider>
+        </HabitsProvider>
+      </LoginProvider>
+    </RegisterProvider>
+  );
+};
 
 export default Providers;
