53944cfccf649ed9c545fa1538b13447b1e722f1
Lucas Gasque <lucasfdg@hotmail.com>
2021-10-20 14:16:54 -0300

    build:criada a pagina group-search

--- src/pages/GroupSearch/style.js
+++ src/pages/GroupSearch/style.js
@@ -1,5 +1,46 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    padding-bottom: 65px;
+    
+    ul{
+        margin: 20px 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        list-style: none;
+    }
 
+    li{
+        margin: 5px 0;
+    }
+
+    @media (min-width: 768px){
+        padding-left: 268px ;
+    }
+`
+
+export const SearchBox = styled.div`
+    height: 68px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--purple);
+
+    h1{
+        display: none;
+    }
+
+    @media (min-width: 768px){
+        height: 156.567px;
+        flex-direction: column;
+        justify-content: space-evenly;
+        
+        h1{
+            display: block;
+            color:var(--white);
+        }
+    }
 `
--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,13 +1,20 @@
 import { createTheme } from '@mui/material/styles'
 
 export const muiTheme = createTheme({
-    palette: {
-        type: 'light',
-        primary: {
-          main: '#6C63FF',
-          light: '#8982FF',
-          dark: '#4B45B2',
-          contrastText: '#FCFCFC'
-        },
+  palette: {
+    type: 'light',
+      primary: {
+        main: '#6C63FF',
+        light: '#8982FF',
+        dark: '#4B45B2',
+        contrastText: '#FCFCFC'
+      },
+      secondary: {
+        main: '#FCFCFC',
+        light: '#FFF',
+        dark: '#B0B0B0',
+        contrastText:'#6C63FF'
+      }
     }    
-})
+  }
+)
