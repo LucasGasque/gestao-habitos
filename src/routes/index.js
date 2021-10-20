5b34fdf30aae124e4e131884963d9bb9cc2fa6d7
Lucas Gasque <lucasfdg@hotmail.com>
2021-10-20 18:36:37 -0300

    fix:arrumada rota MyGroups

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Profile from "../pages/Profile";
+import MyGroups from '../pages/MyGroups'
 
 const Routes = () => {
   return (
@@ -18,6 +19,9 @@ const Routes = () => {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/my-groups">
+          <MyGroups />
         </Route>
         <Route path="/group">
           <Group />
