0812bca0fb2cc59b532c5be8f191760cc37bfdb1
Lucas Gasque <lucasfdg@hotmail.com>
2021-10-21 08:06:39 -0300

    fix: fix na estilização do header e login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,15 +14,16 @@ const Header = () => {
         <h1 onClick={() => sendTo("/")}>Make it Habit!</h1>
         <div>
           <Button
-            className="groundPurple"
-            variant="outlined"
+            sx={{
+              marginRight: '10px',
+            }}
+            variant="contained"
             color="primary"
             onClick={() => sendTo("/login")}
           >
             Login
           </Button>
           <Button
-            className="groundWhite"
             variant="outlined"
             color="primary"
             onClick={() => sendTo("/register")}
